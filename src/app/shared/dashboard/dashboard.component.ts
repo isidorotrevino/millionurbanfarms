62d756ac09ebbcd60674e1fc34bb080987170219
Ana Ortega <aortega@asgbpm.com>
2019-09-02 17:00:16 -0500

    Versión Final

--- src/app/shared/dashboard/dashboard.component.ts
+++ src/app/shared/dashboard/dashboard.component.ts
@@ -18,7 +18,7 @@ export class DashboardComponent implements OnInit {
     if(this.storageService.getUsername() === "client@test.com"){
       this.items = [
         {name:"Buy", route:"../comprar_cliente", icon:"fas fa-carrot fa-5x icon-option"},
-        {name:"Records", route:"../historial_cliente", icon:"fas fa-file-invoice-dollar fa-5x icon-option"},
+        {name:"Record", route:"../historial_cliente", icon:"fas fa-file-invoice-dollar fa-5x icon-option"},
         {name:"Status", route:"../status", icon:"fas fa-chart-bar fa-5x icon-option"}
       ]
     }
@@ -28,7 +28,7 @@ export class DashboardComponent implements OnInit {
         {name:"Buy", route:"../comprar_productor", icon:"fas fa-cart-plus fa-5x icon-option"},
         {name:"Orders", route:"../pedidos_productor", icon:"fas fa-cash-register fa-5x icon-option"},
         {name:"Add", route:"../registrar_producto", icon:"fas fa-plus-circle fa-5x icon-option"},
-        {name:"Records", route:"../historial_productor", icon:"fas fa-file-invoice-dollar fa-5x icon-option"},
+        {name:"Record", route:"../historial_productor", icon:"fas fa-file-invoice-dollar fa-5x icon-option"},
         {name:"Products", route:"../productos", icon:"fas fa-th-list fa-5x icon-option"},
         {name:"Finance You", route:"../financiate", icon:"far fa-money-bill-alt fa-5x icon-option"},
         {name:"Funding", route:"../financiamiento", icon:"fas fa-university fa-5x icon-option"},
